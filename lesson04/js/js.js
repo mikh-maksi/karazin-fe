7885084cb2f7f2a50362e89b31c7e67e481856aa
Mikhail <newlevelcenter@gmail.com>
2021-09-22 16:19:33 +0300

    calc

--- lesson04/js/js.js
+++ lesson04/js/js.js
@@ -1,11 +1,21 @@
-
 send.addEventListener("click",fnc);
 function fnc(){
     let val_a = a.value;
     let val_b = b.value;
-    
-    let val_c = parseInt(val_a) + parseInt(val_b);
-    
+    let val_c = 0
+    if (act.value == '1' ){
+        val_c = parseInt(val_a) + parseInt(val_b);
+    }
+    if (act.value == '2' ){
+        val_c = parseInt(val_a) - parseInt(val_b);
+    }
+    if (act.value == '3' ){
+        val_c = parseInt(val_a) * parseInt(val_b);
+    }
+    if (act.value == '4' ){
+        val_c = parseInt(val_a) / parseInt(val_b);
+    }    
     cc.value = val_c;
+
 }
 
