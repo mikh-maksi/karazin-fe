2a8d0768898d4dfe92eab0975912bfd32ce9efcb
Mikhail <newlevelcenter@gmail.com>
2021-09-22 16:33:59 +0300

    percent

---
+++ lesson04/percents/js/js.js
@@ -0,0 +1,12 @@
+send.addEventListener("click",fnc);
+function fnc(){
+    let val_deposit = deposit.value;
+    let val_percent = percent.value;
+    let val_term = term.value;
+    let val_result = val_deposit;
+    for (let i = 0; i<val_term;i++){
+        val_result = (1+val_percent/100)*val_result
+    }
+    result.innerHTML = val_result;
+
+}
