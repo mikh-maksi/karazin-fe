334f4c02032df8f5de33e385a4149b192c655203
Mikhail <newlevelcenter@gmail.com>
2021-10-06 14:55:32 +0300

    lesson-06-cycles

--- lesson04/percents/js/js.js
+++ lesson04/percents/js/js.js
@@ -4,8 +4,8 @@ function fnc(){
     let val_percent = percent.value;
     let val_term = term.value;
     let val_result = val_deposit;
-    for (let i = 0; i<val_term;i++){
-        val_result = (1+val_percent/100)*val_result
+    for (let i = 0; i<val_term; i++){
+        val_result *= (1+val_percent/100)
     }
     result.innerHTML = val_result;
 
