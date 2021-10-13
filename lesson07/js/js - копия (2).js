d3a53389c136f5534db196ceb6da6755aec16d8b
Mikhail <newlevelcenter@gmail.com>
2021-10-13 15:54:46 +0300

    lesson07

---
+++ lesson07/js/js - копия (2).js
@@ -0,0 +1,7 @@
+b = 0;
+for (i =0; i < 10; i++) {
+    // a = parseInt(Math.random()*10)+20;
+    a = parseInt(prompt("Please Enter a"));
+    b+=a;
+}
+console.log(b);
---
+++ lesson07/js/js - копия.js
@@ -0,0 +1,7 @@
+sum = 1
+for(let i = 1;i<=10;i++){
+    if (i%3==0){
+        sum*=i;
+    }
+}
+console.log(sum);
