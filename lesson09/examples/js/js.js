defa8aafdcf2079967614ad79dba077372e179d0
mikh-maksi <newlevelcenter@gmail.com>
2021-10-20 15:51:13 +0300

    functions2

--- lesson09/examples/js/js.js
+++ lesson09/examples/js/js.js
@@ -4,3 +4,33 @@ function add(a,b){
 }
 
 console.log(add(4,5))
+
+
+function check_answer(arr,n,answer){
+    let flag  = false;
+    arr.forEach((element, index) => { 
+        if (index == n){
+            if (element == answer){
+                flag = true;
+            }
+        }
+     } )
+    return flag;
+}
+answers_arr = [1,2,1,3,4]
+console.log(check_answer(answers_arr,0,2));
+
+
+function check_answer1(arr,n,answer){
+    let flag = false;
+    arr.forEach((element,index) =>{
+        if (index == n){
+            if (element == answer){
+                flag = true;
+            }
+        }
+    })
+    return flag;
+}
+
+console.log(check_answer1(answers_arr,0,2))
