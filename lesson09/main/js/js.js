2f46c572dd19c91605d75e72a6746cb9a468e135
mikh-maksi <newlevelcenter@gmail.com>
2021-10-20 16:24:15 +0300

    functions4

--- lesson09/main/js/js.js
+++ lesson09/main/js/js.js
@@ -2,8 +2,10 @@ let arr = [1,5,3,6,8,4,3];
 let questions =["Сколько будет 2+2==4","2+5","4+3","3/3"];
 let answers = [["3","4","5","true"],["1","2","7","9"],["1","2","7","9"],["1","2","7","9"]]
 let right_answers=[3,2,2,0]
+let answered = [0,0,0,0]
 
 current_question = 0
+total_points = 0;
 
 function render(n_q){
     question.innerHTML=questions[n_q];
@@ -27,18 +29,23 @@ function prev(){
 function check(){
     n = document.getElementsByName("question");
     k=0;
+    if (answered[current_question]==0){
+
     n.forEach(element => {
         if (element.checked){
-            if (k == right_answers[0]){
-                console.log("right");
+            if (k == right_answers[current_question]){
+                alert("right");
+                total_points++;
             }
             else{
-                console.log("wrong");
+                alert("wrong");
             }
         }
     k++;
     });
-
+    }
+    answered[current_question] = 1;
+    console.log(total_points);
 }
 
 
