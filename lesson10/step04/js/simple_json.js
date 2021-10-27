a042bb7c98ebfb13fc708646550b52fe5784dfe6
mikh-maksi <newlevelcenter@gmail.com>
2021-10-27 16:02:57 +0300

    step04

---
+++ lesson10/step04/js/simple_json.js
@@ -0,0 +1,9 @@
+fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5').then(response => response.json())
+    .then(function (ttt) {
+        console.log(ttt);
+        console.log(ttt[0]);
+        console.log(ttt[0].buy+ ' '+ ttt[0].sale);
+        buy.innerHTML=ttt[0].buy;
+        sale.innerHTML = ttt[0].sale;
+       // alert(ttt[0].ccy)
+    })
