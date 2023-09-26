c26ab7680d4e4f6f17a72b956a934ed7ca145b4e
Mikhail <newlevelcenter@gmail.com>
2023-09-26 15:51:37 +0300

    lesson04_task02

---
+++ lesson04/02task_question/js/js2.js
@@ -0,0 +1,10 @@
+btn.addEventListener("click",fnc);
+function fnc (){
+    console.log(a1.checked)
+    console.log(a2.checked)
+    console.log(a3.checked)
+    console.log(a4.checked)
+   if (a2.checked){
+       alert("You - win!")
+   }
+}
