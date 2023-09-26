03f016458052afb94cb011d56999b5c42caeeaab
Mikhail <newlevelcenter@gmail.com>
2023-09-26 16:08:17 +0300

    lesson04_task04

---
+++ lesson04/04task_digit/js/js4.js
@@ -0,0 +1,13 @@
+btn.addEventListener("click",fnc);
+
+function fnc(){
+    a = parseInt(digit.value);
+    console.log(a);
+    if (a%2==0){
+        result.innerHTML = a+" Кратно 2";
+    }
+    else{
+        result.innerHTML = a+" Не Кратно2";
+    }
+
+}
