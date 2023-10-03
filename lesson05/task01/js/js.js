1a1857e4fde7b9542ecaa6f65dbc4f1fee42bc00
Mikhail <newlevelcenter@gmail.com>
2023-10-03 16:13:17 +0300

    task01

---
+++ lesson05/task01/js/js.js
@@ -0,0 +1,6 @@
+let questions = ['Питання 1','Питання 2','Питання 3','Питання 4']; //Ваші оцінки за екзамени в останню сесію.
+
+
+for (let i = 0;i<questions.length;i=i+1){
+    console.log(questions[i]);
+}
